de00178eb2cd35e2c09f6ec2ec244ca9b29f2267
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-10 14:13:00 +0600

    update route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
-const HomePage = async() => {
-  const res= await fetch('http://localhost:5000/blogs',{
-    next:{
-      revalidate: 30
-    }
-  })
-  const blogs=await res.json();
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const blogs = await res.json();
+  // console.log(blogs);
   return (
     <div className="my-10">
-      <LatestBlogs blogs={blogs}/>
+      <LatestBlogs blogs={blogs} />
     </div>
   );
 };
